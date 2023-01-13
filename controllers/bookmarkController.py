527f0e03b8a857a626681588c7d49b39a6df6806
sktp06 <kantaporn_s@cmu.ac.th>
2023-01-14 01:26:08 +0700

    fix: addBookmark method

--- controllers/bookmarkController.py
+++ controllers/bookmarkController.py
@@ -2,6 +2,7 @@ from flask import jsonify, request
 from models.bookmark import Bookmark
 from flask_sqlalchemy import SQLAlchemy
 import pickle
+import json
 db = SQLAlchemy()
 parsed_data = pickle.load(open('assets/parsed_data.pkl', 'rb'))
 
@@ -17,9 +18,9 @@ class BookmarkController:
                 raise
             animes = []
             for b in bookmarks:
-                animes.append(parsed_data[parsed_data['mal_id'] == b['anime']].to_dict('records')[0])
-            result = {'userId': userId, 'animes': animes}
-            return jsonify(result)
+                temp = parsed_data[parsed_data['mal_id'] == b['anime']].to_dict('records')[0]
+                animes.append({'mal_id': temp['mal_id'], 'title': temp['title'], 'images': temp['images'], 'score': b['score']})
+            return jsonify({'animes': animes}), 200
         except:
             return jsonify({'message': 'The bookmark for userId {} is not existed'.format(userId)})
 
@@ -35,7 +36,7 @@ class BookmarkController:
                 db.session.add(bookmark)
                 db.session.commit()
             except:
-                return jsonify({'message': 'This bookmark information went wrong'}), 404
+                return jsonify({'message': 'This bookmark information went wrong'}), 500
             return jsonify({'message': 'The bookmark is added successfully'})
         except:
             return jsonify({'message': 'The request body required userId, animeId and score'}), 400
